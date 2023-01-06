52886e028be661e44894c6c44c36b10daae85ff2
Andrew Liakhnovich <hifocr@gmail.com>
2023-01-06 07:22:41 +0300

    Новый проект

---
+++ Models/DataWorker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManager.Models
+{
+    public static class DataWorker
+    {
+        //TODO: Создание и редактирование клиентов, заявок и статусов
+    }
+}
